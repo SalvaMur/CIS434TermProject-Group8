d4e1fc39c62e04d65d140fe97b722d6df4a74d98
Salvador Murillo <salmur2k@gmail.com>
2023-04-17 16:23:49 -0400

    Basic menu screen items added

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import tkinter as tk
+from tkinter import ttk
+
+# Font styles
+LARGE_FONT = 'Arial 40 bold'
 
 # Main menu screen frame
 class MainMenu(tk.Frame):
@@ -7,11 +11,33 @@ class MainMenu(tk.Frame):
         self.master = master
         
         self.master.geometry('800x600') # Change window geometry
-        self.createMenu()
+        self.createMenu() # Create screen UI
         
     def createMenu(self):
-        self.play_button = tk.Button(master=self, text='PLAY', command=self.goToGame)
-        self.play_button.grid(row=0, column=0)
+        self.menuLabel = tk.Label(master=self, font=LARGE_FONT, pady=40)
+        self.menuLabel['text'] = 'Chess'
+        self.menuLabel.pack()
+
+        # Frame that holds buttons and dropdown list
+        self.selFrame = tk.Frame(master=self, pady=40)
+
+        self.pvp_button = tk.Button(master=self.selFrame, command=self.goToGame)
+        self.pvp_button['text'] = 'Player vs Player'
+        self.pvp_button.grid(row=0, column=0, padx=20)
+
+        # WIP -----------------------------------------------
+        self.pvai_button = tk.Button(master=self.selFrame, command=None)
+        self.pvai_button['text'] = 'Player vs AI'
+        self.pvai_button.grid(row=0, column=1, padx=20)
+
+        # Dropdown list of AI difficulty. Defaults to Normal
+        self.botDifficulty_list = ttk.Combobox(master=self.selFrame, state='readonly')
+        self.botDifficulty_list['values'] = ('Easy', 'Normal', 'Hard')
+        self.botDifficulty_list.current(1)
+        self.botDifficulty_list.grid(row=0, column=2, padx=20)
+
+        # Pack selection frame to Menu screen
+        self.selFrame.pack()
         
     def goToGame(self):
         GameScreen(master=self.master).pack()
@@ -24,7 +50,7 @@ class GameScreen(tk.Frame):
         self.master = master
 
         self.master.geometry('1365x768') # Change window geometry
-        self.createGame()
+        self.createGame() # Create screen UI
         
     def createGame(self):
         self.play_button = tk.Button(master=self, text='MENU', command=self.goToMenu)
