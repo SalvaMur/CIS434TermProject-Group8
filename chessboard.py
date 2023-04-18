4220cc3d38f7c53d74b9cdff0facd19e133cd4ce
Salvador Murillo <salmur2k@gmail.com>
2023-04-18 18:14:25 -0400

    Added selection highlight

--- chessboard.py
+++ chessboard.py
@@ -12,9 +12,12 @@ GRAY = '#474747'
 GREEN = '#6ba649'
 WHITE = '#f0f1ff'
 DARK_WHITE = '#e6e6e6'
+MUSTARD = '#e0d79b'
 LIME = '#9fe04f'
 RED = '#e81a3c'
+DARK_RED = '#ab132c'
 BLUE = '#3f59b5'
+PUKE_GREEN = '#84b53f'
 
 class Chessboard(tk.Frame):
     def __init__(self, master):
@@ -26,8 +29,10 @@ class Chessboard(tk.Frame):
         self.board = {}
         self.boardCanvas = tk.Canvas(master=self, width=712, height=712, bg=GREEN, borderwidth=0, highlightthickness=0)
         self.boardCanvas.pack(expand=True)
+        self.boardCanvas.bind('<Button-1>', self.onClick)
         
         self.pieces = {}
+        self.selPiece = None # Keeps track on what piece is selected. Modified by piece class
         
         self.createBoard()
         self.createPieces()
@@ -35,6 +40,8 @@ class Chessboard(tk.Frame):
     # Construct 8x8 chessboard
     def createBoard(self):
         SQUARE_SIZE = 89
+        self.markImage = []
+
         for i in range(8): # Row
             self.board[i+1] = {}
 
@@ -73,22 +80,78 @@ class Chessboard(tk.Frame):
             8:{1:'Rook', 2:'Knight', 3:'Bishop', 4:'Queen', 5:'King', 6:'Bishop', 7:'Knight', 8:'Rook'}
         }
 
-        self.imageRef = []
+        self.pieceImage = []
         for i in startPos:
             color = 'B' if (i <= 2) else 'W'
             for j in startPos[i]:
-                self.imageRef.append(tk.PhotoImage(file=f'img/{color}_{startPos[i][j]}.png'))
+                self.pieceImage.append(tk.PhotoImage(file=f'img/{color}_{startPos[i][j]}.png'))
                 pieceTag = self.boardCanvas.create_image(
                     self.board[i][j]['center']['x'], self.board[i][j]['center']['y'], 
-                    image=self.imageRef[len(self.imageRef) - 1]
+                    image=self.pieceImage[len(self.pieceImage) - 1]
                 )
-                piece = self.boardCanvas.find_withtag(pieceTag)[0]
+                pieceRef = self.boardCanvas.find_withtag(pieceTag)[0]
+                piece = Piece(pieceRef, startPos[i][j], color, self.boardCanvas, i, j)
 
-                self.board[i][j]['piece'] = Piece(piece, startPos[i][j], color, self.boardCanvas, i, j)
-                self.board[i][j]['pieceRef'] = piece
+                self.pieces[pieceTag] = piece
+
+                self.board[i][j]['piece'] = piece
+                self.board[i][j]['pieceRef'] = pieceRef
                 self.board[i][j]['pieceName'] = startPos[i][j]
                 self.board[i][j]['pieceColor'] = color
 
                 if (startPos[i][j] == 'Rook'):
                     self.board[i][j]['defendedByBlack'] = False
                     self.board[i][j]['defendedByWhite'] = False
+
+    def onClick(self, event):
+        tag = self.boardCanvas.find_withtag('current')[0]
+
+        if (tag in self.pieces and self.pieces[tag] is not self.selPiece and 
+            self.pieces[tag].color is 'W'):
+
+            if (self.selPiece is not None):
+                row = self.selPiece.row
+                col = self.selPiece.col
+                oldSquare = self.board[row][col]['square']
+                self.boardCanvas.itemconfig(oldSquare, fill=WHITE if ((row + col) % 2 == 0) else BLUE)
+
+            self.selPiece = self.pieces[tag]
+
+            # WIP ---------------------------------------------------------------------------
+            for i in self.board:
+                for j in self.board:
+                    square = self.board[i][j]['square']
+                    piece = self.board[i][j]['piece']
+
+                    if (piece is not None and self.selPiece is piece):
+                        self.boardCanvas.itemconfig(square, fill=LIME)
+
+                    # A piece is present on square
+                    elif (piece is not None and piece.color == 'B'):
+                        if ((i + j) % 2 == 0):
+                            self.boardCanvas.itemconfig(square, fill=RED)
+
+                        else:
+                            self.boardCanvas.itemconfig(square, fill=DARK_RED)
+
+                    elif (piece is None):
+                        if ((i + j) % 2 == 0):
+                            self.boardCanvas.itemconfig(square, fill=MUSTARD)
+
+                        else:
+                            self.boardCanvas.itemconfig(square, fill=PUKE_GREEN)
+
+            print(f'{self.selPiece.color}_{self.selPiece.type}, Row: {self.selPiece.row}, Col: {self.selPiece.col}')
+        
+        else:
+            self.selPiece = None
+
+            for i in self.board:
+                for j in self.board[i]:
+                    square = self.board[i][j]['square']
+                    if ((i + j) % 2 == 0):
+                            self.boardCanvas.itemconfig(square, fill=DARK_WHITE)
+                    else:
+                            self.boardCanvas.itemconfig(square, fill=BLUE)
+
+            print('Selected empty square')
--- piece.py
+++ piece.py
@@ -1,16 +1,12 @@
-import tkinter as tk
-
 class Piece:
     def __init__(self, pieceRef, type, color, canvasRef, row, col):
         self.pieceRef = pieceRef
         self.type = type
-        self.owner = color
+        self.color = color
         self.canvasRef = canvasRef
         self.row = row
         self.col = col
         self.isFirstMove = True
 
-        self.canvasRef.tag_bind(self.pieceRef, '<ButtonPress-1>', self.onSelect)
-
-    def onSelect(self, event):
-        print(f'{self.owner}_{self.type}, Row: {self.row}, Col: {self.col}')
+    def moveTo(self):
+        None
